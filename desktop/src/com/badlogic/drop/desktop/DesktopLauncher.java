3eeb433e7c5e6f4ad49ff0e61c61e8e4ef109ca1
zalaniz2 <zachalaniz2@gmail.com>
2021-03-21 23:02:12 -0500

    initial commit

---
+++ desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.badlogic.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.drop.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Drop(), config);
+	}
+}
